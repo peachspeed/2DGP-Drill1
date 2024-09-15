c6235de6ea03134bb2e70f6863843758bc719978
peachspeed@note <peachspeed04@gmail.com>
2024-09-16 02:17:37 +0900

    Drill 1 제출 (2023184025 이승화)

---
+++ turtle_4_wasd.py
@@ -0,0 +1,34 @@
+import turtle
+import random
+
+t = turtle
+
+
+        
+def move_w():
+    t.setheading(90)
+    t.forward(50)
+    t.stamp()
+
+def move_a():
+    t.setheading(180)
+    t.forward(50)
+    t.stamp()
+
+def move_s():
+    t.setheading(-90)
+    t.forward(50)
+    t.stamp()
+
+def move_d():
+    t.setheading(0)
+    t.forward(50)
+    t.stamp()
+
+t.shape('turtle')
+
+t.onkey(move_w, 'w')
+t.onkey(move_a, 'a')
+t.onkey(move_s, 's')
+t.onkey(move_d, 'd')
+t.listen()
